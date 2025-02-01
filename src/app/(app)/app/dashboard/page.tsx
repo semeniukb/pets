d6ba6e24540cd32ac99497485dfa88a874744d07
b.semeniuk <bohdansem14@gmail.com>
2025-02-01 13:39:34 +0100

    add app layound and create components for dashboard

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,3 +1,14 @@
+import Branding from "@/components/branding";
+import Stats from "@/components/stats";
+
 export default function Page() {
-  return <div>Dashboard page</div>;
+  return (
+    <main>
+      <div className="flex items-center justify-between text-white py-8">
+        <Branding />
+
+        <Stats />
+      </div>
+    </main>
+  );
 }
