902453bb5b898cdb721592ae2720e39155f149da
b.semeniuk <bohdansem14@gmail.com>
2025-03-31 18:27:21 +0200

    add logg for webhook error

--- src/app/api/stripe/route.tsx
+++ src/app/api/stripe/route.tsx
@@ -11,6 +11,7 @@ export async function POST(request: Request) {
   try {
     event = stripe.webhooks.constructEvent(body, signature!, process.env.STRIPE_WEBHOOK_SECRET!);
   } catch (error) {
+    console.log("Webhook verification failed", error);
     return Response.json(null, { status: 400 });
   }
 
